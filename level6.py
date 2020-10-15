3018e7ed306c3de887dc26903ef4150faad92223
Ben Constable <ben-constable@outlook.com>
2020-10-15 12:54:30 +0100

    Add Level 6-11 Templates

---
+++ level6.py
@@ -0,0 +1,12 @@
+""" Hackathon - Level 6 """
+
+def calculator(a, operator, b):
+    # Add your solution here. You can use additional functions if need be.
+    # Don't forget to add a DocString for all your functions and comment your code.
+    # Your functions should return values rather than printing the result although you can use printing for testing purposes.
+
+if __name__ == '__main__':
+    # Add any code to test your solution here
+    # As per the example, this should return 2
+    print(calculator(10, '/', 5))
+    
